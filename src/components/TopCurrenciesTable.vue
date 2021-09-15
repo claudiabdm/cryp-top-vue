bdf2ae08ad822f33366020d2440c0c77feb6d9e1
claudiabdm <claudiabdm@hotmail.es>
2021-09-15 19:21:59 +0200

    Add Top Currencies Table component

---
+++ src/components/TopCurrenciesTable.vue
@@ -0,0 +1,91 @@
+<template>
+  <BaseTable :rows="currencies" :columns="$options.columns">
+    <template v-slot:name="{ row }">
+      <div class="top-crypto-table__name">
+        <img
+          class="top-crypto-table__logo"
+          :src="`https://www.cryptocompare.com/${row.logo}`"
+          alt=""
+          srcset=""
+          width="24"
+          height="24"
+        />
+        <span>
+          {{ row.name }}
+        </span>
+      </div>
+    </template>
+    <template v-slot:change24h="{ row }">
+      <span
+        :class="[
+          {
+            up: !row.change24h.includes('-'),
+            down: row.change24h.includes('-'),
+          },
+        ]"
+      >
+        {{ row.change24h }}
+      </span>
+    </template>
+  </BaseTable>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import BaseTable from '@/components/BaseTable.vue';
+import { Currency } from '@/types/models';
+
+export default defineComponent({
+  components: {
+    BaseTable,
+  },
+  columns: {
+    name: {
+      name: 'Name',
+      align: 'left',
+    },
+    price: {
+      name: 'Price',
+      align: 'right',
+    },
+    marketCap: {
+      name: 'Market Cap',
+      align: 'right',
+    },
+    change24h: {
+      name: 'Change (24h)',
+      align: 'right',
+    },
+    volume24h: {
+      name: 'Volume (24h)',
+      align: 'right',
+    },
+  },
+  props: {
+    currencies: {
+      type: Array as PropType<Currency[]>,
+      default: () => null,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/mixins/mixins' as *;
+
+.top-crypto-table {
+  &__name {
+    @include flex(center, flex-start);
+  }
+  &__logo {
+    margin-right: rem(10px);
+  }
+}
+
+.up {
+  color: var(--secondary);
+}
+.down {
+  color: var(--tertiary);
+}
+</style>
