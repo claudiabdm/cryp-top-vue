f22b53dc35b3aae2633ce1b08990fecd74958bc3
claudiabdm <claudiabdm@hotmail.es>
2021-09-15 19:20:58 +0200

    Add loading spinner

---
+++ src/components/BaseLoadingSpinner.vue
@@ -0,0 +1,47 @@
+<template>
+  <svg
+    class="svg"
+    width="60"
+    height="60"
+    viewBox="0 0 60 60"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <title>Loading spinner</title>
+    <circle cx="30" cy="30" r="25" stroke="#eee" />
+    <g class="rotate">
+      <circle class="circle" cx="30" cy="30" r="25" stroke="currentColor" />
+    </g>
+  </svg>
+</template>
+
+<style scoped>
+.svg {
+  stroke-linecap: round;
+  stroke-width: 2;
+}
+.rotate {
+  transform-origin: center;
+  animation: rotate 3s infinite linear;
+}
+.circle {
+  stroke-dasharray: 157;
+  stroke-dashoffset: 157;
+  animation: dash 3s infinite ease-in-out;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes dash {
+  100% {
+    stroke-dashoffset: -157;
+  }
+}
+</style>
