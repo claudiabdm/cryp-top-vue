2f28449710a16db1c4ecb12eadc768ee829bfe4a
claudiabdm <claudiabdm@hotmail.es>
2021-09-16 19:26:37 +0200

    Change select element position

--- src/components/BaseSelectCurrencyQuote.vue
+++ src/components/BaseSelectCurrencyQuote.vue
@@ -1,14 +1,19 @@
 <template>
-  <select
-    class="select"
-    v-model="currencyQuote"
-    name="selectCurrencyQuote"
-    id="selectCurrencyQuote"
-  >
-    <option v-for="currency in CURRENCIES" :key="currency" :value="currency">
-      {{ currency }}
-    </option>
-  </select>
+  <div class="select">
+    <label class="select__label" for="selectCurrencyQuote"
+      >Currency quote</label
+    >
+    <select
+      class="select__select"
+      v-model="currencyQuote"
+      name="selectCurrencyQuote"
+      id="selectCurrencyQuote"
+    >
+      <option v-for="currency in CURRENCIES" :key="currency" :value="currency">
+        {{ currency }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script lang="ts">
@@ -26,13 +31,24 @@ export default defineComponent({
 <style lang="scss" scoped>
 @use '../styles/mixins/mixins' as *;
 .select {
-  border: 0;
-  border-radius: rem(10px);
-  padding: rem(5px);
-  background-color: #fff;
-  box-shadow: 0 0 rem(5px) rem(2px) var(--grey-200);
-  &:hover {
-    cursor: pointer;
+  @include flex(center, center, column);
+  width: rem(110px);
+  &__label {
+    color: var(--grey-500);
+    font-size: rem(14px);
+    font-weight: 300;
+  }
+  &__select {
+    width: 100%;
+    border: 0;
+    border-radius: rem(10px);
+    margin-top: rem(5px);
+    padding: rem(5px);
+    background-color: #fff;
+    box-shadow: 0 0 rem(5px) rem(2px) var(--grey-200);
+    &:hover {
+      cursor: pointer;
+    }
   }
 }
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,43 +1,43 @@
 <template>
   <header class="header">
+    <h1 class="header__title header__elem">
+      <RouterLink class="header__title-link" to="/">CrypTop</RouterLink>
+    </h1>
     <div class="header__row">
-      <h1 class="header__title header__elem">
-        <RouterLink class="header__link" to="/">CrypTop</RouterLink>
-      </h1>
-      <BaseSelectCurrencyQuote class="header__elem" />
-    </div>
-    <nav class="header__nav breadcrumbs">
-      <RouterLink
-        class="breadcrumbs__link"
-        to="/"
-        exact-active-class="breadcrumbs__link--active"
-      >
-        Top 10 currencies
-      </RouterLink>
-      <template v-if="$route.path !== '/'">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          class="breadcrumbs__separator"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 5l7 7-7 7"
-          />
-        </svg>
+      <nav class="header__nav breadcrumbs header__elem">
         <RouterLink
           class="breadcrumbs__link"
-          :to="$route.path"
+          to="/"
           exact-active-class="breadcrumbs__link--active"
         >
-          {{ $route.params.symbol }}
+          Top 10 currencies
         </RouterLink>
-      </template>
-    </nav>
+        <template v-if="$route.path !== '/'">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="breadcrumbs__separator"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+          <RouterLink
+            class="breadcrumbs__link"
+            :to="$route.path"
+            exact-active-class="breadcrumbs__link--active"
+          >
+            {{ $route.params.symbol }}
+          </RouterLink>
+        </template>
+      </nav>
+      <BaseSelectCurrencyQuote class="header__select header__elem" />
+    </div>
   </header>
 </template>
 
@@ -56,25 +56,29 @@ export default defineComponent({
 .header {
   padding: rem(10px);
   &__row {
-    @include flex(center, space-between);
+    @include flex(flex-start, space-between);
     width: 100%;
-    flex-wrap: wrap;
+    flex-wrap: wrap-reverse;
   }
   &__elem {
-    margin-bottom: clamp(1rem, 2.5vw, 5vw);
+    margin-top: clamp(1rem, 2.5vw, 5vw);
   }
   &__title {
     @include flex(center, flex-start);
     color: var(--primary-dark);
   }
-  &__link {
-    font-size: clamp(2rem, 2vw, 3vw);
+  &__title-link {
+    font-size: clamp(2.5rem, 2vw, 3vw);
     font-weight: 700;
+  }
+  &__select {
+    margin-right: 0;
+    margin-left: auto;
   }
 }
 .breadcrumbs {
   @include flex(center, flex-start);
-  @include size(100%, rem(18px));
+  @include size(auto, rem(18px));
   margin-top: rem(20px);
   font-size: 1rem;
   &__link {
